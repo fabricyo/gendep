f50b15eb823716dba6558d771d817c87736f3ede
Fabricyo Figueira <fabricyofigueira@hotmail.com>
2022-10-04 18:44:10 -0300

    Update Item.php

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -11,6 +11,6 @@ class Item extends Model
     protected $table = 'itens';
 
     protected $fillable = [
-        'nome', 'marca', 'categoria', 'local', 'barcode', 'entrada', 'saida', 'validade'
+        'nome', 'qtd', 'marca', 'categoria', 'local', 'barcode', 'entrada', 'saida', 'validade'
     ];
 }
